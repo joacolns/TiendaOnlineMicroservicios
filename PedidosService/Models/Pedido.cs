63a82fa696d1dd96d78cddbe20c477d37844f24f
njoaco <joaquinlucasnieto@gmail.com>
2025-06-20 20:32:50 -0300

    PedidosService now working

---
+++ PedidosService/Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PedidosService.Models
+{
+    public class Pedido
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public string Estado { get; set; }
+        
+    }
+}
