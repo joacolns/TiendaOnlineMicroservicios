4169865c1d2b2df2cc65e8d2d8d0e0eddac87f65
njoaco <joaquinlucasnieto@gmail.com>
2025-06-20 20:02:39 -0300

    payment service now working

---
+++ PagoService/Controller/PagosController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PagoService.Data;
+using PagoService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PagoService.Controller
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class PagosController : ControllerBase
+    {
+        private readonly PagosDbContext _context;
+
+        public PagosController(PagosDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pago>> GetAll()
+        {
+            return Ok(_context.Pagos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Pago> GetPago(int id)
+        {
+            var pago = _context.Pagos.Find(id);
+            return pago is not null ? Ok(pago) : NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<Pago> CrearPago([FromBody] Pago nuevoPago)
+        {
+            _context.Pagos.Add(nuevoPago);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetPago), new { id = nuevoPago.Id }, nuevoPago);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult EliminarPago(int id)
+        {
+            var pagoAEliminar = _context.Pagos.Find(id);
+            if (pagoAEliminar is null) return NotFound();
+
+            _context.Pagos.Remove(pagoAEliminar);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ PagoService/Models/Pago.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PagoService.Models
+{
+    public class Pago
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Metodo { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.UtcNow;
+    }
+}
